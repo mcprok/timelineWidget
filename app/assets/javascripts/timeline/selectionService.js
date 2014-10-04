847c6b3a4735f76152dc22b1cc78b5b1453e988d
mcprok <maciej.prokopiuk@schibsted.pl>
2014-10-04 19:45:05 +0200

    Added event details modal.

--- app/assets/javascripts/timeline/selectionService.js
+++ app/assets/javascripts/timeline/selectionService.js
@@ -2,21 +2,26 @@ define('timeline/selectionService',function (require) {
     var alertsService = require('alerts/alertsService');
 
     var selectCallback = function (timeline) {
-        // TODO - Change the bootstrap popover for something else
-        // or leave bootstrap but adjust styles for it. It could be good option. :)
+
         var sel = timeline.getSelection();
         if (sel.length) {
             if (sel[0].row != undefined) {
                 var row = sel[0].row;
                 var item = timeline.getItem(row);
-                var elem = $('.timeline-event-content'); // element selected - probably can be done somehow different
-                elem.popover( {
-                    content: $('#popover_content_wrapper').html(),
-                    placement: "top",
-                    toogle: "popover",
-                    html: true
+
+                $('#event-title').find('> .event-content').text(item.content);
+
+                var $eventData = $('#modal-event-data');
+                $eventData.find('.event-start > .event-content').text(item.start);
+                $eventData.find('.event-end > .event-content').text(item.end);
+                $eventData.find('.event-location > .event-content').text(item.location);
+                $eventData.find('.event-location > .geo-x').text(item.geo[0]);
+                $eventData.find('.event-location > .geo-y').text(item.geo[1]);
+                $eventData.find('.event-description > .event-content').text(item.description);
+
+                $('#detailsModal').modal({
+                        "backdrop" : "static"
                 });
-                console.log(elem);
             }
         }
     };
