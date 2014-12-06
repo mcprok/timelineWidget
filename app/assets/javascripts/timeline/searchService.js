1bd5360cf39d810ab2eb6c9b1d8cf8084e22e895
Grzegorz Miejski <grz.miejski@gmail.com>
2014-12-06 13:56:20 +0100

    Items search results in table.

--- app/assets/javascripts/timeline/searchService.js
+++ app/assets/javascripts/timeline/searchService.js
@@ -1,6 +1,5 @@
 define("timeline/searchService", function() {
 
-
     var search = function(searchString, items) {
         var searchResults = [];
         for ( var i = 0 ; i < items.length ; i++ ) {
@@ -9,12 +8,6 @@ define("timeline/searchService", function() {
                 searchResults.push(item);
             }
         }
-        var $searchResults = $('#searchResults');
-        $searchResults.html("");
-        _.each(searchResults, function(item) {
-            $searchResults.append($('<span>'+ item.content+ '</span><br>'));
-        });
-        console.log(searchResults);
         return searchResults;
     };
 
