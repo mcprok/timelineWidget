e689699ef84a4b869e860ac072c74d2ec6f57696
Grzegorz Miejski <grz.miejski@gmail.com>
2014-12-09 17:23:13 +0100

    Hardcoded hiding and showing group of events.

--- app/assets/javascripts/timeline/timelineService.js
+++ app/assets/javascripts/timeline/timelineService.js
@@ -1,4 +1,4 @@
-define('timeline/timelineService',function (require) {
+define('timeline/timelineService', function (require) {
     var alertsService = require('alerts/alertsService');
     var selectionService = require('timeline/selectionService');
     var newEventService = require('forms/newEventForm');
@@ -9,10 +9,12 @@ define('timeline/timelineService',function (require) {
     var $timeline = null;
     var newEventId = 0;
 
+    var hiddenGroups = {};
+
     var createTimeline = function (containerId, data, options) {
 
         var container = $('#' + containerId)[0];
-        if ( $timeline == null) {
+        if ($timeline == null) {
             $timeline = new links.Timeline(container);
             $timeline.draw(data, options);
             timePointer.init($timeline);
@@ -27,7 +29,7 @@ define('timeline/timelineService',function (require) {
         links.events.addListener($timeline, 'delete', onEventDelete);
         alertsService.publishInfoAlert('Timeline created!');
 
-        links.events.addListener($timeline, 'select',function() {
+        links.events.addListener($timeline, 'select', function () {
             selectionService.selectCallback($timeline)
         });
 
@@ -52,19 +54,19 @@ define('timeline/timelineService',function (require) {
         alertsService.publishInfoAlert('Event deleted: ' + deletedEvent.content);
     };
 
-    var addEventHandler = function(type, callback) {
-        if ($timeline == null || type == null || type.length > 0 ) {
+    var addEventHandler = function (type, callback) {
+        if ($timeline == null || type == null || type.length > 0) {
             console.error('Cannot add on selection handler - no timeline or data provided');
         }
         links.events.addListener($timeline, type, callback);
     };
 
-    var canCreateGroup = function(groupName) {
+    var canCreateGroup = function (groupName) {
         return !groupExists(groupName);
     };
 
-    var groupExists = function( groupName) {
-        if ( $timeline == null) {
+    var groupExists = function (groupName) {
+        if ($timeline == null) {
             return false;
         } else {
             return $timeline.groups.some(function (group) {
@@ -75,9 +77,80 @@ define('timeline/timelineService',function (require) {
         }
     };
 
-    var search =  function(searchString) {
+    var search = function (searchString) {
         return searchService.search(searchString, $timeline.items);
     };
+
+    var hideGroup = function (groupName) {
+        var items = $timeline.items;
+
+        var remaining = [];
+        var toHide = [];
+
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            if (item.group.content != groupName) {
+                remaining.push(item);
+            } else {
+                toHide.push(item);
+            }
+        }
+
+        var newGroups = [];
+        _.forEach($timeline.groups, function(group) {
+            console.log(group);
+           if ( group.content != groupName ) {
+               newGroups.push(group);
+           }
+        });
+        $timeline.groups = newGroups;
+//        delete $timeline.groups[1];
+        hiddenGroups[groupName] = toHide;
+
+        _.forEach(remaining, function(event) {
+           event.group = event.group.content;
+        });
+
+        $timeline.setData(remaining);
+        $timeline.repaintGroups();
+        console.log(remaining);
+        $timeline.redraw();
+    };
+
+    var showGroup = function (groupName) {
+        if (hiddenGroups.hasOwnProperty(groupName)) {
+            var newElements = hiddenGroups[groupName];
+            _.forEach(newElements, function(event) {
+               event.group = groupName;
+            });
+            $timeline.addItems(newElements);
+            $timeline.redraw();
+        } else {
+            console.log('Hidden group doesnt not exist: ' + groupName);
+        }
+    };
+
+    var deleteGroup = function (groupName) {
+        console.log($timeline);
+
+        var items = $timeline.items;
+
+        var remaining = [];
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            if (item.group.content != groupName) {
+                remaining.push(item);
+            }
+        }
+
+        delete $timeline.groups[0];
+
+        $timeline.setData(remaining);
+        console.log(remaining);
+
+        $timeline.redraw();
+        console.log($timeline);
+    }
 
     return {
         createTimeline: createTimeline,
@@ -85,7 +158,10 @@ define('timeline/timelineService',function (require) {
         getTimeline: getTimeline,
         addEventHandler: addEventHandler,
         canCreateGroup: canCreateGroup,
-        search: search
+        search: search,
+        deleteGroup: deleteGroup,
+        showGroup: showGroup,
+        hideGroup: hideGroup
     };
 
 });
